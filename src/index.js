10df313fcc64cb67fe7a5bb3cc39cc3c24fdb818
Tyson Hester <ironman9967@gmail.com>
2017-08-13 03:56:26 -0400

    getting initial list of prebuilds on app start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('armb-1 app')
+
+import each from 'lodash/fp/each'
+import rp from 'request-promise'
+
+rp({
+	uri: `${process.env.CLOUD_URI}/api/bin/downloader/prebuild`,
+	headers: { 'User-Agent': 'iron-iot-armb-1' },
+	json: true
+})
+	.then(console.log)
+
+	// .then(each(prebuild => getFromCloud(prebuild)))
