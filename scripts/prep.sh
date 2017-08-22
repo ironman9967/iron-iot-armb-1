5b90d9352d99b7c711b96fedc8c9455bdd6f873f
Tyson Hester <ironman9967@gmail.com>
2017-08-22 12:14:29 -0400

    wip

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -15,13 +15,28 @@ nvm alias default "$nodeVersion"
 echo "setting nvm to use $nodeVersion"
 nvm use "$nodeVersion"
 
-echo "getting app for armb 1"
-wget -O $APP_PATH/get-app.sh "https://raw.githubusercontent.com/ironman9967/iron-iot-common/master/scripts/get-app.sh"
-source $APP_PATH/get-app.sh $APP_PATH armb 1
+echo "getting app version from $CLOUD_URI/api/bin/versions/armb/1/app/version"
+version=`wget -qO- $CLOUD_URI/api/bin/versions/armb/1/app/version`
+echo "app version is $version"
 
-source $APP_PATH/common/scripts/build-app.sh $version armb 1
+buildTar=built_armb-1_app_$version.tar.gz
+echo "trying to download $version built from $CLOUD_URI/bin/devices/builds/armb/1/app/$builtTar"
+wget -O $APP_PATH/$buildTar $CLOUD_URI/bin/devices/builds/armb/1/app/$builtTar`
 
-rm -rf $APP_PATH/get-app.sh
+if [ $? == 0 ]
+then
+	echo "built release $version downloaded successfully"
+
+else
+	echo "$version not built"
+	echo "getting app for armb 1"
+	wget -O $APP_PATH/get-app.sh "https://raw.githubusercontent.com/ironman9967/iron-iot-common/master/scripts/get-app.sh"
+	source $APP_PATH/get-app.sh $APP_PATH armb 1
+
+	source $APP_PATH/common/scripts/build-app.sh $version armb 1
+
+	rm -rf $APP_PATH/get-app.sh
+fi
 
 echo "starting $repo app"
 chmod +x $APP_PATH/common/scripts/start.sh
