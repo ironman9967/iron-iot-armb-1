bbf8e1744039bb66e9ee70422629438575cbe8c0
Tyson Hester <ironman9967@gmail.com>
2017-08-21 23:59:17 -0400

    fixed logger undefined

--- src/http-server/api/index.js
+++ src/http-server/api/index.js
@@ -3,9 +3,11 @@ import { createPrebuildReadyApi } from './prebuild-ready'
 
 export const routeApi = ({
 	server,
+	logger,
 	downloadPrebuild
 }) => {
 	const { createRoute: prebuildReadyRoute } = createPrebuildReadyApi({
+		logger,
 		downloadPrebuild
 	})
 	server.route(prebuildReadyRoute())
